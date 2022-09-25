f26af9fdd39ad21ae605757d201981f4994c19f2
Grusl <ffhshdxva@gmail.com>
2022-09-25 20:24:15 +0300

    Fixed cap attachment

--- js/header.js
+++ js/header.js
@@ -1,15 +1,18 @@
 const header = document.getElementsByTagName("header")[0];  // Получаем шапку
+const header_ = document.getElementById("header");  // Получаем заменитель шапки
 let position = 0;  // Позиция прилипания шапки
 
 
 function header_logic() {
     if (window.pageYOffset >= header.offsetTop && header.offsetTop !== 0)  // Шапка должна прилипнуть?
     {
+        header_.classList.add("active");
         position = header.offsetTop;
         header.classList.add("sticky");
     } else if (position > window.pageYOffset)  // Шапка должна отлипнуть?
     {
         header.classList.remove("sticky");
+        header_.classList.remove("active");
     }
 }
 
